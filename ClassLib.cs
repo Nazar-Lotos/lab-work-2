02c08c5c28ca619a4e50b81946fcbf52a62de44f
Nazar Lotos <lototskyinazar@gmail.com>
2024-10-16 15:54:21 +0300

    Добавьте файлы проекта.

---
+++ ClassLib.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace Lab2Lib
+{
+    public class Television
+    {
+        public string Brand;
+        public string Model;
+        public int ScreenSize;
+        public string Resolution;
+        public bool IsSmartTV;
+        public int SoundPower;
+        public bool IsOn;
+        public int Brightness;
+
+        public void TurnOn()
+        {
+            IsOn = true;
+            Console.WriteLine("Телевізор увімкнено.");
+        }
+
+        public void TurnOff()
+        {
+            IsOn = false;
+            Console.WriteLine("Телевізор вимкнено.");
+        }
+
+        public void AdjustBrightness(int level)
+        {
+            if (level < 0 || level > 100)
+            {
+                Console.WriteLine("Рівень яскравості повинен бути від 0 до 100.");
+            }
+            else
+            {
+                Brightness = level; 
+                Console.WriteLine($"Яскравість встановлена на рівні {Brightness}%.");
+            }
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Бренд: {Brand}");
+            Console.WriteLine($"Модель: {Model}");
+            Console.WriteLine($"Діагональ екрану: {ScreenSize}\"");
+            Console.WriteLine($"Роздільна здатність: {Resolution}");
+            Console.WriteLine($"Smart TV: {(IsSmartTV ? "Так" : "Ні")}");
+            Console.WriteLine($"Потужність звуку: {SoundPower} Вт");
+            Console.WriteLine($"Стан: {(IsOn ? "Увімкнено" : "Вимкнено")}");
+            Console.WriteLine($"Яскравість: {Brightness}");
+        }
+    }
+}
